8c09057736f75dc2b9d0c53c83bd6871e0fb925d
E1kari <josefine.cholle@STUD.HOCHSCHULE-HEIDELBERG.DE>
2025-01-17 00:17:10 +0100

    Automatic Respawn & Invincibility

--- Projektarbeit III/Assets/Scripts/Health.cs
+++ Projektarbeit III/Assets/Scripts/Health.cs
@@ -3,9 +3,21 @@ using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [SerializeField]
+    private bool invincible = false;
+
     public virtual void takeDamage()
     {
+        if (invincible) return;
         gameObject.GetComponent<SpriteRenderer>().color = Color.red;
-        Destroy(gameObject, 0.1f);
+        if (gameObject.tag == "Player")
+        {
+            GameObject levelStart = GameObject.FindWithTag("Level Start");
+            levelStart.GetComponent<Level_Start>().spawnPlayer();
+        }
+        else
+        {
+            Destroy(gameObject, 0.5f);
+        }
     }
 }
