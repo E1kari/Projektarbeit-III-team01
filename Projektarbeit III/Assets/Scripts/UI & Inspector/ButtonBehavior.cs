3192be16cfa7dde017f9f447a9b064eab61d8818
E1kari <josefine.cholle@STUD.HOCHSCHULE-HEIDELBERG.DE>
2025-02-13 13:57:18 +0100

    option Menu

--- Projektarbeit III/Assets/Scripts/UI & Inspector/ButtonBehavior.cs
+++ Projektarbeit III/Assets/Scripts/UI & Inspector/ButtonBehavior.cs
@@ -1,9 +1,24 @@
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
 using UnityEngine.SceneManagement;
 
 public class ButtonBehavior : MonoBehaviour
 {
+    private GameObject[] panels_;
+
+    public void Start()
+    {
+        panels_ = GameObject.FindGameObjectsWithTag("Panel");
+        foreach (GameObject panel in panels_)
+        {
+            if (panel.name == "Control Panel")
+            {
+                panel.SetActive(true);
+            }
+            else panel.SetActive(false);
+        }
+    }
     public void loadScene(SceneAsset pa_scene)
     {
         if (pa_scene == null)
@@ -15,6 +30,20 @@ public class ButtonBehavior : MonoBehaviour
         string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePath);
 
         SceneManager.LoadScene(sceneName);
+    }
+
+    public void activatePanel(GameObject pa_panel)
+    {
+        if (pa_panel == null)
+        {
+            Debug.LogError("Panel is not set");
+            return;
+        }
+        foreach (GameObject panel in panels_)
+        {
+            panel.SetActive(false);
+        }
+        pa_panel.gameObject.SetActive(true);
     }
 
     public void exitGame()
